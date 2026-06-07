2676fd3c3d8ffd88ce6cf0fc36b0177e1b5a8717
Biswaprakash Sahoo <codebiswaprakash07@gmail.com>
2026-06-07 18:30:41 +0530

    Update useSocket.js

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useCallback, useReducer } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
+const SOCKET_URL = import.meta.env.VITE_SERVER_URL;
 
 const initState = {
   connected:false, me:null,
